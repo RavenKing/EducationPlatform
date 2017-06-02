6bf52c7cfb1649d178f567687b63b5e5b6f2175e
RavenKing <kevinyang.chengkai@gmail.com>
2017-06-02 20:19:41 +0800

    done

--- Components/Mypage/XMGMypage.js
+++ Components/Mypage/XMGMypage.js
@@ -12,15 +12,16 @@ import {
   View
 } from 'react-native';
 import Button from 'antd-mobile/lib/button';
+import InputItem from 'antd-mobile/lib/input-item';
 
 export default class Mypage extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          My Page
-        </Text>
-        <Button size='small' type='primary'> 点我 </Button>
+              <InputItem size='small' label="good"  type = "phone "placeholder="shit" title="nice"/>
+              <Text>shwt</Text><InputItem size='small' label="good"  type = "phone "placeholder="shit" title="nice"/>
+              <InputItem size='small' label="good"  type = "phone "placeholder="shit" title="nice"/>
+               <Button size='small' type='primary'> 点我 </Button>
       </View>
     );
   }
